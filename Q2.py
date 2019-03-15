c3a1b2c7062f734ab390786021c0d119dd7905e4
haozhang <332915343@qq.com>
2019-03-15 18:38:24 -0500

    Initial commit

---
+++ Q2.py
@@ -0,0 +1,30 @@
+import pandas
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.models import Model
+from keras.layers import Input
+
+
+
+# load dataset
+from sklearn.model_selection import train_test_split
+import pandas as pd
+dataset = pd.read_csv("Breas Cancer.csv", header=None, dtype='float').values
+
+# print
+import numpy as np
+X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,0:30], dataset[:,30],
+                                                    test_size=0.25, random_state=87)
+np.random.seed(155)
+my_first_nn = Sequential() # create model
+my_first_nn.add(Dense(20, input_dim=30, activation='relu')) # hidden layer
+my_first_nn.add(Dense(32, activation='linear')) #Dense layer 1
+my_first_nn.add(Dense(21, activation='tanh'))
+my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+
+
+my_first_nn.compile(loss='mean_squared_error', optimizer='adam')
+my_first_nn_fitted = my_first_nn.fit(X_train, Y_train, epochs=100, verbose=0,
+                                     initial_epoch=0)
+print(my_first_nn.summary())
+print(my_first_nn.evaluate(X_test, Y_test, verbose=0))
